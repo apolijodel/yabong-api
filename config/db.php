61dc5e685f14b357e77cda421f48101caaccdc61
apolijodel <apoli_jodel@plpasig.edu.ph>
2026-05-19 18:29:11 +0800

    Updated

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 
-define('DB_HOST', 'sql308.infinityfree.com');
-define('DB_USER', 'if0_41945413');
-define('DB_PASS', 'Apoli2006');
-define('DB_NAME', 'if0_41945413_yabong_db');
+define('DB_HOST', $_ENV['MYSQLHOST']);
+define('DB_USER', $_ENV['MYSQLUSER']);
+define('DB_PASS', $_ENV['MYSQLPASSWORD']);
+define('DB_NAME', $_ENV['MYSQLDATABASE']);
+define('DB_PORT', $_ENV['MYSQLPORT']);
 
 function getDB() {
 
