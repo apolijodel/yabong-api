18bef376ac1c92801388b73d32e6ee421d891011
apolijodel <apoli_jodel@plpasig.edu.ph>
2026-05-19 14:17:08 +0800

    Updated Railway database

--- config/db.php
+++ config/db.php
@@ -1,23 +1,34 @@
 <?php
-// Online Database connection - InfinityFree
-define('DB_HOST', 'sql308.infinityfree.com');
-define('DB_USER', 'if0_41945413');
-define('DB_PASS', 'Apoli2006');
-define('DB_NAME', 'if0_41945413_yabong_db');
+
+define('DB_HOST', $_ENV['MYSQLHOST']);
+define('DB_USER', $_ENV['MYSQLUSER']);
+define('DB_PASS', $_ENV['MYSQLPASSWORD']);
+define('DB_NAME', $_ENV['MYSQLDATABASE']);
+define('DB_PORT', $_ENV['MYSQLPORT']);
 
 function getDB() {
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    $conn = new mysqli(
+        DB_HOST,
+        DB_USER,
+        DB_PASS,
+        DB_NAME,
+        DB_PORT
+    );
 
     if ($conn->connect_error) {
         http_response_code(500);
+
         echo json_encode([
             'success' => false,
             'message' => 'Database connection failed: ' . $conn->connect_error
         ]);
+
         exit;
     }
 
     $conn->set_charset('utf8mb4');
+
     return $conn;
 }
 ?>
